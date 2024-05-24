09fbe791edf62c024f15693811340f88dde646c1
Victor Silva Guedes <sguedes.victor@gmail.com>
2024-05-24 18:38:47 -0300

    feat: wrapper

---
+++ src/wrapper/Wrapper.java
@@ -0,0 +1,14 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int numero = 1;
+        float numero2 = 1.5F;
+        boolean booleano = false;
+
+        // As Wrapper Classes permitem realizar operações e manipulações mais avançadas com os dados
+        Float tipoFloat = 1F;
+        Boolean tipoBoolean = true;
+        Integer tipoInteiro = 1;
+    }
+}
